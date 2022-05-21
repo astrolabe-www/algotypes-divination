321326c9b30f56606fcd301d62f38b6edacd073a
Thiago Hersan <thiago.hersan+github@gmail.com>
2022-05-21 19:52:22 -0400

    adds algorithm descriptions in EN

--- html/js/cards.js
+++ html/js/cards.js
@@ -7,7 +7,7 @@ const CARDS = [
       pt: "Gerador de Números Pseudoaleatórios",
     },
     algorithm: {
-      en: "",
+      en: "Random number sequences where all numbers appear with the same frequency are extremely important for encryption. But computer-generated random numbers aren't really random because computers are deterministic and predictable devices. The algorithms used to generate random numbers try to overcome this impossibility by generating a stream of evenly distributed numbers whose order is difficult to predict.",
       pt: "Sequências de números aleatórios onde todos os números aparecem com a mesma frequência são extremamente importantes para a criptografia. Mas números aleatórios gerados por computadores não são realmente aleatórios porque computadores são dispositivos determinísticos e previsíveis. Os algoritmos usados para gerar números aleatórios tentam superar essa impossibilidade gerando um fluxo de números uniformemente distribuídos cuja ordem é difícil de prever.",
     },
     message: {
@@ -23,7 +23,7 @@ const CARDS = [
       pt: "Transformação Rápida de Fourier",
     },
     algorithm: {
-      en: "",
+      en: "In mathematics, a Fourier analysis is a way of expressing any sequence of numbers by summing simpler periodic functions. The Fast Fourier Transform is an algorithm used to do this analysis and navigate between two representations of the same information: one that shows when certain things happen and another that expresses how often.",
       pt: "Na matemática, uma análise de Fourier, ou análise harmônica, é uma maneira de expressar qualquer sequência de números através da soma de funções trigonométricas mais simples e periódicas. A Transformação Rápida de Fourier é um algoritmo usado para fazer essa análise e navegar entre duas representações da mesma informação: uma que mostra quando certas coisas acontecem e outra que expressa com que frequência.",
     },
     message: {
@@ -39,7 +39,7 @@ const CARDS = [
       pt: "PageRank",
     },
     algorithm: {
-      en: "",
+      en: "PageRank is the link analysis algorithm used by Google to rank pages in search results. It is a way of measuring the relevance of a website by counting the quantity and quality of links that point to it.",
       pt: "PageRank é o algoritmo de análise de links usado pelo Google para classificar as páginas nos resultados de pesquisa. É uma forma de medir a relevância de um website contando a quantidade e a qualidade dos links apontados para ele.",
     },
     message: {
@@ -55,7 +55,7 @@ const CARDS = [
       pt: "Reação-Difusão",
     },
     algorithm: {
-      en: "",
+      en: "The reaction–diffusion algorithm proposed by Alan Turing to model two-component chemical reactions can also be used to describe different dynamic processes found in nature and generate physical and visual patterns. Such a system is able to generate spatially periodic patterns that are uniform but not perfectly repeatable.",
       pt: "O algoritmo de reação-difusão proposto por Alan Turing originalmente para descrever reações químicas entre duas substâncias também pode ser usado para modelar outros processos dinâmicos encontrados na natureza. Muitas vezes é utilizado para gerar diferentes tipos de efeitos visuais porque é capaz de gerar padrões periódicos que não são perfeitamente simétricos ou repetitivos.",
     },
     message: {
@@ -71,7 +71,7 @@ const CARDS = [
       pt: "Poda Alfa-Beta",
     },
     algorithm: {
-      en: "",
+      en: "Alpha-Beta Pruning is an algorithm used in computer games to choose the optimal move for a player. It optimizes the process of enumerating all possible moves by removing (pruning) all the moves that are not optimal or won't have an effect on the final outcome of a game.",
       pt: "A Poda Alfa-Beta é um algoritmo usado em programas de jogos para escolher o lance ideal. Ele otimiza o processo de listagem de todos os movimentos possíveis, removendo todos aqueles que não são ideais ou não resultarão no resultado desejado ao final do jogo.",
     },
     message: {
@@ -87,7 +87,7 @@ const CARDS = [
       pt: "Quicksort",
     },
     algorithm: {
-      en: "",
+      en: "In computer science, a sorting algorithm is an algorithm that puts a set of elements in a certain order (which frequently means numerical or alphabetical order). Quicksort is a divide-and-conquer algorithm that works by partitioning the elements to be sorted into two sets, and then recursively sorting each of the two subsets.",
       pt: "Na ciência da computação, um algoritmo de ordenação é um algoritmo de manipulação de dados que coloca um conjunto de elementos em uma certa ordem (frequentemente ordem numérica ou alfabética). O Quicksort é um algoritmo que funciona recursivamente, dividindo os elementos a serem ordenados em dois conjuntos e, em seguida, organizando separadamente cada um dos dois subconjuntos.",
     },
     message: {
@@ -103,7 +103,7 @@ const CARDS = [
       pt: "Máximo Divisor Comum",
     },
     algorithm: {
-      en: "",
+      en: "The greatest common divisor (GCD) of two numbers is the largest number that divides both of them without leaving a remainder. An algorithm for calculating the GCD was first described by Euclid around 300 BC and is one of the earliest examples of an algorithm: a step-by-step procedure for performing a calculation according to well-defined rules.",
       pt: "O máximo divisor comum (MDC) de dois números é o maior número que os divide sem deixar sobra. Um algoritmo para encontrar o MDC de dois números foi descrito por Euclides por volta de 300 aC e é um dos primeiros exemplos de uma descrição de um procedimento para realizar um cálculo de acordo com regras bem definidas.",
     },
     message: {
@@ -119,7 +119,7 @@ const CARDS = [
       pt: "Travessia de Grafos e Labirintos",
     },
     algorithm: {
-      en: "",
+      en: "Maze traversal algorithms can be used to solve navigation and mapping problems. These algorithms generally utilize two types of search strategies: one that explores one path at a time, going as far as possible before backtracking, and another where it explores multiple paths at the same time by taking one step at a time in each possible direction.",
       pt: "Algoritmos de travessia são utilizados para automatizar sistemas de navegação e mapeamento. Geralmente esses algoritmos utilizam duas estratégias de busca: uma que prioriza a busca por um caminho de cada vez, indo o mais longe possível antes de retroceder, e outra que explora vários caminhos ao mesmo tempo, dando um passo de cada vez em cada direção possível.",
     },
     message: {
@@ -135,7 +135,7 @@ const CARDS = [
       pt: "Árvore Splay",
     },
     algorithm: {
-      en: "",
+      en: "In computer science, a binary search tree (BST) is a type of data structure for storing data. BSTs allow for fast element lookups because data is organized in a way that decreases the total number of comparisons made while searching for a specific element. Splay Trees are special structures where frequently accessed elements are even quicker to find during subsequent searches.",
       pt: "Na ciência da computação, uma árvore binária é um tipo de estrutura usada para organizar dados. Elas permitem buscas rápidas porque os dados são organizados de uma forma que diminui o número de comparações feitas durante a busca por um elemento específico. As Árvores Splay são árvores especiais, onde os elementos acessados com frequência são ainda mais fáceis de encontrar.",
     },
     message: {
@@ -151,7 +151,7 @@ const CARDS = [
       pt: "SHA3-512",
     },
     algorithm: {
-      en: "",
+      en: 'The SHA3-512 algorithm has many applications in information security and cryptography, notably in digital signatures and password systems for user authentication. It\'s an example of a hash function: an algorithm that transforms messages of arbitrary size into fixed-size "hashes" or "digests". This is a one-way transformation, meaning that it\'s practically impossible to recover the original message from its hash.',
       pt: 'Esse algoritmo é muito utilizado em aplicações de segurança e criptografia, e em sistemas de assinaturas digitais, armazenamento de senhas e autenticação. É um exemplo de um algoritmo de "resumo": algoritmos que são capazes de transformar mensagens de tamanho arbitrário em resumos de tamanho fixo através de uma transformação de mão única. Ou seja, é praticamente impossível recuperar a mensagem original a partir do seu resumo.',
     },
     message: {
@@ -167,7 +167,7 @@ const CARDS = [
       pt: "Ruído Perlin",
     },
     algorithm: {
-      en: "",
+      en: "Perlin Noise is a technique used to produce natural appearing textures on computer generated graphics. The algorithm is able to represent the complexity of natural phenomena to create visual effects. It's often used to make computer-generated elements, such as fire, smoke, or clouds, appear more natural, by imitating the controlled random appearance of textures in nature.",
       pt: "O Ruído Perlin é uma técnica usada para produzir texturas de aparência natural em gráficos gerados por computador. O algoritmo permite a representação da complexidade dos fenômenos naturais em efeitos visuais. É frequentemente usado para criar elementos visuais como fogo, fumaça e nuvens, imitando a aparência aleatória e natural de elementos e texturas.",
     },
     message: {
@@ -183,7 +183,7 @@ const CARDS = [
       pt: "Caixeiro-Viajante",
     },
     algorithm: {
-      en: "",
+      en: "The Travelling Salesperson Problem is an optimization problem which seeks the shortest round trip through a collection of cities. Algorithms that solve this kind of problem either take an undetermined amount of time to find the best solution, or find a good solution within a fixed amount of time. There is no efficient method that always finds the best possible solution.",
       pt: "O Caixeiro Viajante é um problema de otimização computacional que tenta determinar a menor rota para percorrer uma série de cidades. Algoritmos que calculam a solução para esse tipo de problema levam um tempo indeterminado para encontrar a melhor solução ou encontram uma boa solução dentro de um período de tempo fixo. Não existe um método eficiente que sempre encontre a melhor solução possível.",
     },
     message: {
@@ -199,7 +199,7 @@ const CARDS = [
       pt: "Curva de Hilbert",
     },
     algorithm: {
-      en: "",
+      en: "The Hilbert-curve is an algorithm for representing a multi‐dimensional space as a one-dimensional space. These curves are like a thread that passes through every point in space, imposing a linear order to the multi‐dimensional space. The Hilbert-curve is important because it is able to preserve certain types of locality: points that are close in multi‐dimensional space will remain close in one-dimensional space.",
       pt: "A Curva de Hilbert é um algoritmo usado para transformar um espaço multidimensional em um espaço unidimensional. Essas curvas são como um fio que passa por todos os pontos de um espaço multidimensional, impondo uma ordem linear a esse espaço. A Curva de Hilbert é importante porque é capaz de preservar certos tipos de localidade: pontos que estão próximos no espaço multidimensional permanecerão próximos no espaço unidimensional.",
     },
     message: {
@@ -215,7 +215,7 @@ const CARDS = [
       pt: "Coletor Mark & Sweep",
     },
     algorithm: {
-      en: "",
+      en: "The Mark & Sweep algorithm is commonly used for automatic memory management. It's the part of a program that is responsible for cleaning up and releasing memory that is occupied by objects that are no longer being used. It optimizes the frequency and efficiency of this process by keeping lists of objects and periodically checking if they are still being used.",
       pt: "O algoritmo Mark & Sweep é usado para fazer gerenciamento automático de memória. É a parte de um programa responsável por limpar e liberar a memória ocupada por código que não está mais sendo usado.",
     },
     message: {
@@ -231,7 +231,7 @@ const CARDS = [
       pt: "Chaves Diffie–Hellman",
     },
     algorithm: {
-      en: "",
+      en: "Asymmetric cryptography is based on the idea that the password/key used to encrypt data can be made public, while the password/key used to decrypt data should be kept private. Encryption with a public key can only be undone by decrypting with its private key. The Diffie–Hellman key exchange is a method of creating and securely exchanging these cryptographic keys.",
       pt: "A criptografia assimétrica é baseada na ideia de que a senha usada para criptografar dados pode ser pública, enquanto a senha usada para descriptografar deve permanecer privada. Dados criptografados com uma chave pública só podem ser lidos usando sua chave privada. A troca de chaves Diffie-Hellman é um método seguro de criar e trocar chaves criptográficas em espaços públicos como a internet.",
     },
     message: {
@@ -247,7 +247,7 @@ const CARDS = [
       pt: "Compressão JPEG",
     },
     algorithm: {
-      en: "",
+      en: "The JPEG compression algorithm exploits physiological properties of human vision to reduce the size of digital image files. The algorithm is composed of a series of steps that decrease color information and high-frequency content by progressively transforming, downsampling and compressing sections of an image.",
       pt: "O algoritmo de compressão JPEG explora propriedades fisiológicas da visão humana para diminuir o tamanho de arquivos de imagem. Comprime imagens por partes, diminuindo a informação sobre a intensidade das cores e ignorando conteúdo de alta frequência.",
     },
     message: {
@@ -263,7 +263,7 @@ const CARDS = [
       pt: "Prova de Trabalho em Blockchain",
     },
     algorithm: {
-      en: "",
+      en: "Proof-of-work (PoW) is a piece of data which is difficult to produce, but easy to verify. It is often generated by random processes with low probability of success, so that a lot of trial and error is required before a valid PoW is generated. For digital currencies, PoW is performed every time a set of transactions (a block) is permanently written onto the blockchain.",
       pt: "Uma Prova de Trabalho é um processo algorítmico cujo cálculo é difícil de executar, mas cuja validade é fácil de verificar. Muitas vezes, é gerado por processos aleatórios com baixa probabilidade de sucesso, de modo que muitas tentativas são necessárias antes que um número válido seja gerado. Para moedas digitais, esses cálculos são executados toda vez que um conjunto de transações é gravado permanentemente no blockchain.",
     },
     message: {
@@ -279,7 +279,7 @@ const CARDS = [
       pt: "CORDIC",
     },
     algorithm: {
-      en: "",
+      en: "Computers are of finite capacity; they can only accurately represent a small range of all possible numbers. They are good at handling whole numbers, but all others (fractions, rational numbers) are an approximation that trades off range for precision. CORDIC is a simple and efficient algorithm that works within these limitations to calculate trigonometric functions, square roots and other mathematical functions that depend on fractions, ratios and irrational numbers.",
       pt: "Computadores só conseguem representar com precisão uma pequena fração de todos os números existentes. São bons em lidar com números inteiros, mas todos os outros tipos de números (frações e números racionais) são representados por aproximações numéricas. CORDIC é um algoritmo simples e eficiente que trabalha dentro dessas limitações para calcular funções trigonométricas, raízes quadradas, logaritmos e outras funções matemáticas que dependem de frações e números irracionais.",
     },
     message: {
@@ -295,7 +295,7 @@ const CARDS = [
       pt: "Encodificação de Viterbi",
     },
     algorithm: {
-      en: "",
+      en: "Commonly used in speech recognition, speech synthesis, and computational linguistics, the Viterbi Algorithm uses statistical and probabilistic analysis to reconstruct messages transmitted through noisy channels. Or, given a certain signal (voice recording) that has probably been corrupted (noisy background), the Viterbi Algorithm can estimate the original message based on the set of all possible inputs (words in a given language).",
       pt: "Comumente usado em programas de reconhecimento de fala, síntese de voz e linguística computacional, o Algoritmo de Viterbi usa análise estatística e de probabilidade para reconstruir mensagens transmitidas em canais ruidosos. Ou, dado um determinado sinal (gravação de voz) que provavelmente foi corrompido (fundo com ruído), o Algoritmo de Viterbi usa uma lista de possiveis sinais de entrada (todas as palavras em um dicionário) para reconstruir a mensagem original.",
     },
     message: {
@@ -311,7 +311,7 @@ const CARDS = [
       pt: "Teste de Primalidade",
     },
     algorithm: {
-      en: "",
+      en: "A prime number is a number that is not evenly divisible by any other number besides itself. A primality test is an algorithm for determining whether a number is prime or if, instead, it can be represented as the product of two smaller natural numbers. Primality test algorithms are capable of speeding up naive trial division by using the fact that all primes greater than 3 are of the form 6k + 1 or 6k - 1.",
       pt: "Um número primo é um número que não é divisível por qualquer outro número além de si mesmo. Um teste de primalidade é um algoritmo que é capaz de calcular se um número é primo ou se pode ser representado pelo produto de dois números naturais menores. Os algoritmos de teste de primalidade são capazes de acelerar esse cálculo usando o fato de que todos os números primos maiores que 3 tem a forma 6k + 1 ou 6k - 1.",
     },
     message: {
@@ -327,7 +327,7 @@ const CARDS = [
       pt: "Fecho Convexo",
     },
     algorithm: {
-      en: "",
+      en: "The convex hull of an object is the smallest convex polygon that can completely circumscribe the object. This is useful in robotics and autonomous navigation systems where the convex hull can be used to avoid collisions, since keeping track of the convex hull is much simpler than keeping track of the exact shapes of objects.",
       pt: "O fecho de um objeto é o menor polígono convexo que pode circunscrever completamente o objeto. Esse tipo de polígono é extremamente útil na robótica e em sistemas de navegação autônoma, porque pode ser usado para evitar colisões, uma vez que acompanhar um fecho convexo é muito mais simples do que acompanhar a forma exata de um objeto.",
     },
     message: {
@@ -343,7 +343,7 @@ const CARDS = [
       pt: "Controlador PID",
     },
     algorithm: {
-      en: "",
+      en: "A proportional–integral–derivative controller (PID controller) is a control loop mechanism that employs feedback to automatically correct aspects of an autonomous system. It is used in autopilot and industrial systems that require continuous monitoring, control and correction. A PID controller algorithm continuously calculates an error value and applies a responsive correction based on previous and current values of this error.",
       pt: "Um Controlador PID é um mecanismo de controle que emprega feedback para corrigir automaticamente ações de um sistema autônomo. É usado em sistemas industriais que requerem monitoramento, controle e correção contínuos. O algoritmo de um Controlador PID calcula continuamente o seu valor de erro, a distância entre seu estado atual e o desejado, e aplica uma correção responsiva com base nas características do histórico desse erro.",
     },
     message: {
